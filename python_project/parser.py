706868db07a4693d1104724e8dc74081a4d2e86b
Chenhui <cliu9@iastate.edu>
2017-02-02 15:04:08 -0600

    Output the parsed detector data

--- python_project/parser.py
+++ python_project/parser.py
@@ -3,7 +3,7 @@ import sys
 import time
 import urllib2
 import xml.etree.ElementTree as ET
-import numpy
+import csv
 
 # Get the root node of the XML hierarchy, and total detector number
 tree=ET.parse('stream.xml')
@@ -11,13 +11,12 @@ root=tree.getroot()
 z=root.getchildren()[2].getchildren()[0].getchildren()[3]
 length = len(z)
 
-f = open('det.csv','w')
-# Get the 'Detector-id' and its 'status' from XML
-f.write('Detector-id'+';'+'Status'+'\n')
+f= open('det.csv','w')
+writer = csv.writer(f) 
+writer.writerow(['ID']+['Status'])
+## Get the ID and status from XML
 for i in range(1,length,1):
-  detid =z.getchildren()[i].getchildren()[0].text
-  status=z.getchildren()[i].getchildren()[1].text
-  f.write(detid+';'+status+'\n')
+         detid =z.getchildren()[i].getchildren()[0].text
+         status=z.getchildren()[i].getchildren()[1].text
+         writer.writerow([detid]+[status]) ## must use [] to make sure the data is a list.OW, the string would be segmented.
 f.close()
- 
-
